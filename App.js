227a518e6d4490fd25e0e40e5ac84cb0db944836
N-Hidaya <nurul-h@hotmail.com>
2022-07-02 21:12:22 +0800

    Added Readme and made changes to app.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView} from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text>Daya's App</Text>
+      <View>
+        <Text>My favourite thing</Text>
+        <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}}
+              style={{width: 200, height: 200}} />
+      </View>
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1}}
+                defaultValue="Enter your favourite thing" />
+    </ScrollView>
   );
 }
 
